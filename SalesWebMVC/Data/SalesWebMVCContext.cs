9534e6b0eb83a8b585e15785549a59f0c5f7efe2
Rafael Johann <rafael.johann2@gmail.com>
2021-05-25 09:34:52 -0300

    Project created

---
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Data
+{
+    public class SalesWebMVCContext : DbContext
+    {
+        public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=12345;Persist Security Info=True;User ID=sa;Initial Catalog=SalesWeb;Data Source=DESKTOP-3JJ46QQ");
+        }
+    }
+}
